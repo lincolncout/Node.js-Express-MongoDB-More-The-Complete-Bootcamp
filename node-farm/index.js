99e6f4dac708dd95707b7a01ebd651b8f344da8e
Lincoln Antunes Nogueira Coutinho <lincolnantunesnogueiracoutinho@MacBook-Air-de-Lincoln.local>
2025-04-27 18:39:27 -0300

    routing, server, file system

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,8 +1,62 @@
 const fs = require("fs"); // fs -> file system (reading and writing data from the system
+const http = require("http");
+const url = require("url");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+/*********** FILES ****************/
 
-const textOut = `This is what we know about avocado: ${textIn}\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written!");
+// Blocking, synchronous way
+
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about avocado: ${textIn}\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+// Non-blocking, asynchronows way
+
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR! 💥");
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written! 😄");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+/************** SERVER ***************/
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+// Criando um servidor
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+// listener (escutando na porta 8000)
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
