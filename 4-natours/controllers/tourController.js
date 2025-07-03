4bf81a9fd83f52cc424e5c35aae19fd9a9b8f79a
Lincoln Antunes Nogueira Coutinho <lincolnantunesnogueiracoutinho@MacBook-Air-de-Lincoln.local>
2025-07-02 21:41:56 -0300

    refatorando o codigo da API

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,5 +1,6 @@
 import path from "path";
 import Tour from "./../models/tourModel.js";
+import APIFeatures from "./../utils/apiFeatures.js";
 
 const __dirname = path.resolve();
 
@@ -14,49 +15,15 @@ export const aliasTopTours = (req, res, next) => {
 export const getAllTours = async (req, res) => {
   try {
     console.log(req.query);
-    // BUILD QUERY
-    // 1A) Filtering
-    const queryObj = { ...req.query };
-    const excludeFields = ["page", "sort", "limit", "fields"];
-    excludeFields.forEach((el) => delete queryObj[el]);
-
-    // 1B) Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|lte|lt|gt)\b/g, (match) => `$${match}`);
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
-
-    // 3) Fields limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v"); // excluindo a versão do mongoDb (todo o resto)
-    }
-
-    // 4) Pagination (importante para limitar a quantidade de registros retonados)
-
-    const page = req.query.page * 1 || 1; // converte de string para inteiro a req
-    const limit = req.query.limit * 1 || 1;
-    const skip = (page - 1) * limit;
-
-    // page=2 & limit=10 1-10, page 1, 11,20, page 2, 21-30 page 3
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error("This page does not exist");
-    }
 
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const tours = await features.query;
 
     // const query = Tour.find()
     //   .where("duration")
@@ -64,6 +31,7 @@ export const getAllTours = async (req, res) => {
     //   .where("duration")
     //   .equals("easy");
 
+    // console.log(tours);
     // SEND RESPONSE
     res.status(200).json({
       status: "sucess",
